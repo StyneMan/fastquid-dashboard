df262d37227f0956a8155529b25b1ac4ec320656
Styneman <stanleynyekpeye@gmail.com>
2024-06-06 21:22:13 +0100

    ne commit

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const baseURL = "http://162.254.32.113:8080"; // "https://fast-quid-api-service.vercel.app";  "https://server.fastquid.ng" // "http://13.48.5.110:8080"; //
+export const baseURL = "https://fast-quid-api-service.vercel.app";  
+// "http://162.254.32.113:8080"; // "https://server.fastquid.ng" // "http://13.48.5.110:8080"; //
 
 const axiosInstance = axios.create({
   baseURL: `${baseURL}/api`,
